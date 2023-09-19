2e4ba6556b259a2cd2ab2d42f02346c0e66a7565
dendersen <dendersen@endersen.dk>
2023-09-19 13:34:34 +0200

    draw now  draws all items

--- src/Client/Controller/Draw.java
+++ src/Client/Controller/Draw.java
@@ -1,9 +1,11 @@
 package Client.Controller;
 
+import Client.Controller.draw.Art;
+import Client.Controller.draw.ArtList;
 import processing.core.PApplet;
 
 public class Draw extends PApplet{
-  
+  ArtList content = new ArtList();
   @Override
   public void settings(){
     
@@ -14,13 +16,15 @@ public class Draw extends PApplet{
   }
   @Override
   public void draw(){
-    
+    for (int i = 0; i < content.size(); i++) {
+      content.get(i).draw(g);
+    }
   }
   /**
    * add an item to the draw pile
    * @return the index atributed to the item, use this index for fast removal of item at a later time.
    */
-  // public int addItem(){
-
-  // }
+  public int addItem(Art item){
+    return content.add(item);
+  }
 }
