01181e6f15e3a232520ea0c430881cf8cac4ba5a
dendersen <dendersen@endersen.dk>
2023-09-19 19:10:56 +0200

    sprite can now draw

--- src/Client/Controller/draw/Sprite.java
+++ src/Client/Controller/draw/Sprite.java
@@ -1,12 +1,19 @@
 package Client.Controller.draw;
 
 import processing.core.PGraphics;
+import processing.core.PImage;
 
 public class Sprite extends Art{
-  
+  int x,y;
+  PImage icon;
+  public Sprite(int x, int y, PImage icon){
+    this.x = x;
+    this.y = y;
+    this.icon = icon;
+  }
   @Override
   public void draw(PGraphics g) {
-    throw new UnsupportedOperationException("Unimplemented method 'draw'");
+    g.image(g, x, y);
   }
   
 }
