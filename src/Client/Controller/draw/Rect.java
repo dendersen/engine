87d18008a3c11b6e21a6589b0c90d7d23643c1eb
dendersen <dendersen@endersen.dk>
2023-09-19 19:08:47 +0200

    added color to rect

--- src/Client/Controller/draw/Rect.java
+++ src/Client/Controller/draw/Rect.java
@@ -5,15 +5,21 @@ import processing.core.PGraphics;
 
 public class Rect extends Art{
   int x,y,w,h;
-  public Rect(int x, int y, int width, int height){
+  int r,g,b;
+  public Rect(int x, int y, int width, int height, int r, int g, int b){
     this.x = x;
     this.y = y;
     this.w = width;
     this.h = height;
+    this.r = r;
+    this.g = g;
+    this.b = b;
   }
   @Override
   public void draw(PGraphics g) {
     g.rectMode(PApplet.CORNER);
-    g.rect(x, y, w, h);
+    g.fill(this.r, this.g, this.b);
+    g.stroke(this.r, this.g, this.b);
+    g.rect(this.x, this.y, this.w, this.h);
   }
 }
