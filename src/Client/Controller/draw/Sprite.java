34c45ca64418fdb2567a1054aa8fec640b820e71
dendersen <dendersen@endersen.dk>
2023-09-19 19:09:04 +0200

    begining work on sprites

---
+++ src/Client/Controller/draw/Sprite.java
@@ -0,0 +1,12 @@
+package Client.Controller.draw;
+
+import processing.core.PGraphics;
+
+public class Sprite extends Art{
+  
+  @Override
+  public void draw(PGraphics g) {
+    throw new UnsupportedOperationException("Unimplemented method 'draw'");
+  }
+  
+}
