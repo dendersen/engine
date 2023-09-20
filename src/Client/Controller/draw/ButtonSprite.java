4dc5756ba57830d6eeee4f4a58ebf6f51627128f
dendersen <dendersen@endersen.dk>
2023-09-20 14:55:44 +0200

    made Button sprite abstract and added abstract on click

--- src/Client/Controller/draw/ButtonSprite.java
+++ src/Client/Controller/draw/ButtonSprite.java
@@ -1,13 +1,17 @@
 package Client.Controller.draw;
 
+import java.util.function.Function;
+
 import Client.App.Button;
 import processing.core.PImage;
 
-public class ButtonSprite extends Sprite{
-
-  public ButtonSprite(int x, int y, PImage icon, Button action) {
+public abstract class ButtonSprite extends Sprite{
+  Button interaction;
+  public ButtonSprite(int x, int y, PImage icon) {
     super(x, y, icon);
-    
+    Function<Integer,Void> func = this::OnClick; 
+    interaction = new Button(x, y,func);
   }
-  
+  private Void OnClick(int ID){onClick(ID);return null;}
+  public abstract void onClick(int ID);
 }
