b5160b7b6fbcd8b4dcfb65374edb6f2d5e3947a9
dendersen <dendersen@endersen.dk>
2023-09-20 14:38:33 +0200

    simple button class built

--- src/Client/App/Button.java
+++ src/Client/App/Button.java
@@ -1,5 +1,22 @@
 package Client.App;
 
+import java.util.ArrayList;
+import java.util.function.Function;
+
 public class Button {
-  
+  int x,y;
+  static ArrayList<Button> click = new ArrayList<Button>();
+  static int ButtonCount = -1;
+  int buttonID;
+  Function<Integer,Void> onClick;
+  public Button(int x, int y, Function<Integer,Void> action){
+    this.x = x;
+    this.y = y;
+    click.add(this);
+    buttonID = ButtonCount++;
+    onClick = action;
+  }
+  public void onClick() {
+    onClick.apply(buttonID);
+  }
 }
