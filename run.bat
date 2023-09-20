0a46e64b3ff8d613fe11d32aca57086d8d78b23a
dendersen <dendersen@endersen.dk>
2023-09-20 16:17:51 +0200

    starting on version

--- run.bat
+++ run.bat
@@ -1,1 +1,7 @@
+@echo off
 echo unfinished and may not work correctly
+javac -source 7 -target 7 -cp ./src/;./lib/* -d ./bin/ ./src/Client/*.java
+echo compiled
+jar -cvfe runnable.jar Client/Start bin/Client/* ./lib/*
+echo jarred
+java -jar runnable.jar
