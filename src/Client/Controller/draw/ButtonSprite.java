54b04f296db759d204622627508e315b72e58d0a
dendersen <dendersen@endersen.dk>
2023-09-20 14:38:46 +0200

    starting on button sprite

---
+++ src/Client/Controller/draw/ButtonSprite.java
@@ -0,0 +1,13 @@
+package Client.Controller.draw;
+
+import Client.App.Button;
+import processing.core.PImage;
+
+public class ButtonSprite extends Sprite{
+
+  public ButtonSprite(int x, int y, PImage icon, Button action) {
+    super(x, y, icon);
+    
+  }
+  
+}
